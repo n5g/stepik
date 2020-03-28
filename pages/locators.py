22cb0d7bbdce48b04fe2b664cb52b9710713eaa7
n5g <n3g@mail.ru>
2020-03-28 18:46:33 +0500

    up

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    #regisration
+    EMAIL_REGISTRATION_FIELD =(By.NAME, "registration-email")
+    PASSWORD_REGISTRATION_FIELD =(By.NAME, "registration-password1")
+    PASSWORD_REPEAT_REGISTRATION_FIELD =(By.NAME, "registration-password2")
+    REGISTRATION_BUTTON = (By.NAME, "registration_submit")
+    #enter
+    ENTER_EMAIL_FIELD =(By.NAME, "login-username")
+    ENTER_PASSWORD_FIELD =(By.NAME, "login-password")
+    ENTER_BUTTON =(By.NAME, "login_submit")
